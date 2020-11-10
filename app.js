a7cb445d3f868f854fd5d411d3b72e711ef4f204
GeronimoJohn <johnageronimo1@gmail.com>
2020-11-10 22:08:30 +1300

    forgot to push javascript features

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 // variables
 const cartItems = document.querySelector(".navbar__cartCount");
 const cartBtn = document.querySelector(".cartButton");
+const size = document.querySelector("size");
+const productS = document.querySelector(".product__small");
+const productM = document.querySelector(".product__medium");
+const productL = document.querySelector(".product__large");
+
+cartBtn.addEventListener("click", addToCart);
+productS.addEventListener("click", updateSize);
+productM.addEventListener("click", updateSize);
+productL.addEventListener("click", updateSize);
+
+function addToCart(event) {
+  event.preventDefault();
+
+  let itemCount = 0;
+
+  cartItems.classList.toggle("total");
+  if (cartItems.classList.contains("total")) {
+    itemCount++;
+  }
+
+  cartItems.innerText = itemCount;
+}
+
+function updateSize(event) {
+  event.preventDefault;
+}
