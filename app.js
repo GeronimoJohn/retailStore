537b3e73f8161667d9317eabd1c564b616462076
GeronimoJohn <johnageronimo1@gmail.com>
2020-11-12 12:43:35 +1300

    Shize feature added

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 // variables
 const cartItems = document.querySelector(".navbar__cartCount");
 const cartBtn = document.querySelector(".cartButton");
-const size = document.querySelector("size");
+const size = document.querySelector(".size");
 const productS = document.querySelector(".product__small");
 const productM = document.querySelector(".product__medium");
 const productL = document.querySelector(".product__large");
 
-cartBtn.addEventListener("click", addToCart);
-productS.addEventListener("click", updateSize);
-productM.addEventListener("click", updateSize);
-productL.addEventListener("click", updateSize);
+productS.addEventListener("click", (event) => {
+  let shirtSize = "S";
+  size.innerText = shirtSize;
+});
+productM.addEventListener("click", (event) => {
+  let shirtSize = "M";
+  size.innerText = shirtSize;
+});
+productL.addEventListener("click", (event) => {
+  let shirtSize = "L";
+  size.innerText = shirtSize;
+});
 
-function addToCart(event) {
-  event.preventDefault();
+let itemAmount = 0;
 
-  let itemCount = 0;
-
-  cartItems.classList.toggle("total");
-  if (cartItems.classList.contains("total")) {
-    itemCount++;
-  }
-
-  cartItems.innerText = itemCount;
-}
-
-function updateSize(event) {
-  event.preventDefault;
-}
+// Everytime we pressed the add to cart it updates the amount of the item
+cartBtn.addEventListener("click", (event) => {
+  itemAmount++;
+  cartItems.innerText = itemAmount;
+});
